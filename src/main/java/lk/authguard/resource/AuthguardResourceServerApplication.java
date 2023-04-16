5f067c112e652066f07600db3a8f19b5159ae73f
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2023-04-16 23:06:34 +0530

    Initialize project

---
+++ src/main/java/lk/authguard/resource/AuthguardResourceServerApplication.java
@@ -0,0 +1,15 @@
+package lk.authguard.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@SpringBootApplication
+@EnableMethodSecurity
+public class AuthguardResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthguardResourceServerApplication.class, args);
+    }
+
+}
